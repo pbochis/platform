a4f4f355089d6433af7c838781be9dfb5c58cd98
Jakob Englisch <jakob.englisch@catalysts.cc>
2015-10-27 17:00:03 +0100

    Fix startup racecondition, and compile warning

--- platform-server-kernel/src/main/java/uno/cod/platform/server/core/config/SecurityConfig.java
+++ platform-server-kernel/src/main/java/uno/cod/platform/server/core/config/SecurityConfig.java
@@ -3,31 +3,39 @@ package uno.cod.platform.server.core.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import uno.cod.platform.server.core.repository.UserRepository;
+import uno.cod.platform.server.core.service.UserDetailsServiceImpl;
 
 @Configuration
-@Order(10)
+@Order(Ordered.HIGHEST_PRECEDENCE)
 @EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
 public class SecurityConfig {
-    private UserDetailsService userDetailsService;
+    private UserRepository userRepository;
 
     @Autowired
-    public void SecurityConfig(UserDetailsService userDetailsService) {
-        this.userDetailsService = userDetailsService;
+    public void SecurityConfig(UserRepository userRepository) {
+        this.userRepository = userRepository;
     }
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(this.userDetailsService).passwordEncoder(this.passwordEncoder());
+        auth.userDetailsService(userDetailsService(userRepository)).passwordEncoder(this.passwordEncoder());
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(UserRepository userRepository) {
+        return new UserDetailsServiceImpl(userRepository);
     }
 }
--- platform-server-kernel/src/main/java/uno/cod/platform/server/core/service/AbstractBaseService.java
+++ platform-server-kernel/src/main/java/uno/cod/platform/server/core/service/AbstractBaseService.java
@@ -14,18 +14,22 @@ public abstract class AbstractBaseService<R extends JpaRepository, E> {
         this.repository = repository;
     }
 
+    @SuppressWarnings("unchecked")
     public E save(E entity) {
         return (E) repository.save(entity);
     }
 
+    @SuppressWarnings("unchecked")
     public List<E> findAll() {
         return repository.findAll();
     }
 
+    @SuppressWarnings("unchecked")
     public E findById(Long id) {
         return (E) repository.findOne(id);
     }
 
+    @SuppressWarnings("unchecked")
     public void delete(Long id) {
         repository.delete(id);
     }
