6514a9ae217fd904ddee3ad939c906e572f1f348
Jakob Englisch <jakob.englisch@catalysts.cc>
2016-03-18 14:27:50 +0100

    Fix checkstyle

--- platform-server-rest/src/main/java/uno/cod/platform/server/rest/controller/IpController.java
+++ platform-server-rest/src/main/java/uno/cod/platform/server/rest/controller/IpController.java
@@ -12,7 +12,6 @@ import java.net.SocketException;
 import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 public class IpController {
