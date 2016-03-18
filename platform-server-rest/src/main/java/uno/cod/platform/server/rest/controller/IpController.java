8e49fc8a78c873e23e13c8b325c1d1c1ca04e6bf
Jakob Englisch <jakob.englisch@catalysts.cc>
2016-03-18 15:22:41 +0100

    Remove old /ip location, moved /ip2 to /ip

--- platform-server-rest/src/main/java/uno/cod/platform/server/rest/controller/IpController.java
+++ platform-server-rest/src/main/java/uno/cod/platform/server/rest/controller/IpController.java
@@ -6,42 +6,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import java.net.InetAddress;
-import java.net.NetworkInterface;
-import java.net.SocketException;
-import java.util.ArrayList;
-import java.util.Enumeration;
-import java.util.List;
-
 @RestController
 public class IpController {
-
     @RequestMapping(value = "/ip", method = RequestMethod.GET)
-    public ResponseEntity<String> ip() throws SocketException {
-        List<InetAddress> addrList = new ArrayList<>();
-        Enumeration<NetworkInterface> networkInterfaces = NetworkInterface.getNetworkInterfaces();
-        while (networkInterfaces.hasMoreElements()) {
-            NetworkInterface ifc = networkInterfaces.nextElement();
-            if (ifc.isUp()) {
-                Enumeration<InetAddress> inetAddresses = ifc.getInetAddresses();
-                while (inetAddresses.hasMoreElements()) {
-                    addrList.add(inetAddresses.nextElement());
-                }
-            }
-        }
-
-        for(InetAddress inetAddress : addrList) {
-            if(inetAddress.isSiteLocalAddress()) {
-                if(inetAddress.getHostAddress().startsWith("10.")) {
-                    return new ResponseEntity<>(inetAddress.getHostAddress(), HttpStatus.OK);
-                }
-            }
-        }
-        return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-    }
-
-    @RequestMapping(value = "/ip2", method = RequestMethod.GET)
-    public ResponseEntity<String> ip2() {
+    public ResponseEntity<String> ip() {
         RestTemplate restTemplate = new RestTemplate();
 
         HttpHeaders headers = new HttpHeaders();
