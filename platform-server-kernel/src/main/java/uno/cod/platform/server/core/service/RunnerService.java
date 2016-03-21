5d1736a979fda7fe11d3f00fd93c1950f20b545d
Victor Balan <victor.balan93@gmail.com>
2016-03-21 11:55:22 +0200

    Provide all endpoints and runners for taskCreate

---
+++ platform-server-kernel/src/main/java/uno/cod/platform/server/core/service/RunnerService.java
@@ -0,0 +1,23 @@
+package uno.cod.platform.server.core.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import uno.cod.platform.server.core.dto.runner.RunnerShowDto;
+import uno.cod.platform.server.core.repository.RunnerRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class RunnerService {
+    private final RunnerRepository repository;
+
+    @Autowired
+    public RunnerService(RunnerRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<RunnerShowDto> findAll() {
+        return repository.findAll().stream().map(RunnerShowDto::new).collect(Collectors.toList());
+    }
+}
