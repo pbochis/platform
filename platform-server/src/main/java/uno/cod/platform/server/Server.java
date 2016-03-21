fe42f0d946dece56b5c5e4e5fc8634c46de1c2bb
Jakob Englisch <jakob.englisch@catalysts.cc>
2016-03-21 13:16:47 +0100

    GAE: additional (unrestricted) connector for ws

--- platform-server/src/main/java/uno/cod/platform/server/Server.java
+++ platform-server/src/main/java/uno/cod/platform/server/Server.java
@@ -1,9 +1,16 @@
 package uno.cod.platform.server;
 
+import org.apache.catalina.connector.Connector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+
+import static uno.cod.platform.server.core.Profiles.APPENGINE;
 
 @SpringBootApplication
 public class Server extends SpringBootServletInitializer {
@@ -15,5 +22,19 @@ public class Server extends SpringBootServletInitializer {
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(Server.class);
+    }
+
+    @Bean
+    @Profile(APPENGINE)
+    public EmbeddedServletContainerFactory servletContainer() {
+        TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory();
+        tomcat.addAdditionalTomcatConnectors(createStandardConnector(8090));
+        return tomcat;
+    }
+
+    private Connector createStandardConnector(int port) {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setPort(port);
+        return connector;
     }
 }
