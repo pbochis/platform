d91353112db64b4648bac49e9f4bc9e59d30e524
Victor Balan <victor.balan93@gmail.com>
2016-03-22 12:13:35 +0200

    Add languages

--- platform-server-kernel/src/main/java/uno/cod/platform/server/core/dto/template/TemplateShowDto.java
+++ platform-server-kernel/src/main/java/uno/cod/platform/server/core/dto/template/TemplateShowDto.java
@@ -1,17 +1,14 @@
 package uno.cod.platform.server.core.dto.template;
 
 import org.springframework.beans.BeanUtils;
-import uno.cod.platform.server.core.domain.Language;
 import uno.cod.platform.server.core.domain.Template;
 
 public class TemplateShowDto {
-    public TemplateShowDto(Template template){
+    public TemplateShowDto(Template template) {
         BeanUtils.copyProperties(template, this);
     }
 
     private Long id;
-
-    private Language language;
 
     public Long getId() {
         return id;
@@ -19,13 +16,5 @@ public class TemplateShowDto {
 
     public void setId(Long id) {
         this.id = id;
-    }
-
-    public Language getLanguage() {
-        return language;
-    }
-
-    public void setLanguage(Language language) {
-        this.language = language;
     }
 }
