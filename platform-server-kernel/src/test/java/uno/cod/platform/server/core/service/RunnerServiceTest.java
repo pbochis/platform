f037204900e20970b671683ec1a07e261b2ce2a7
Victor Balan <victor.balan93@gmail.com>
2016-03-28 14:04:34 +0300

    runner tests

---
+++ platform-server-kernel/src/test/java/uno/cod/platform/server/core/service/RunnerServiceTest.java
@@ -0,0 +1,43 @@
+package uno.cod.platform.server.core.service;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import uno.cod.platform.server.core.domain.Runner;
+import uno.cod.platform.server.core.dto.runner.RunnerShowDto;
+import uno.cod.platform.server.core.repository.RunnerRepository;
+import uno.cod.platform.server.core.service.util.RunnerTestUtil;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.UUID;
+
+import static org.junit.Assert.*;
+
+public class RunnerServiceTest {
+    private RunnerService service;
+    private RunnerRepository repository;
+
+    @Before
+    public void setup(){
+        this.repository = Mockito.mock(RunnerRepository.class);
+        this.service = new RunnerService(repository);
+    }
+
+    @Test
+    public void findAll() throws Exception {
+        List<Runner> runners = Collections.singletonList(RunnerTestUtil.getRunner());
+        Mockito.when(repository.findAll()).thenReturn(runners);
+
+        List<RunnerShowDto> dtos = service.findAll();
+
+        assertEquals(runners.size(), dtos.size());
+
+        RunnerShowDto dto = dtos.get(0);
+        Runner runner = runners.get(0);
+
+        assertEquals(runner.getId(), dto.getId());
+        assertEquals(runner.getName(), dto.getName());
+    }
+}
---
+++ platform-server-kernel/src/test/java/uno/cod/platform/server/core/service/util/RunnerTestUtil.java
@@ -0,0 +1,15 @@
+package uno.cod.platform.server.core.service.util;
+
+import uno.cod.platform.server.core.domain.Runner;
+
+import java.util.UUID;
+
+public class RunnerTestUtil {
+    public static String NAME = "default runner";
+    public static Runner getRunner(){
+        Runner runner = new Runner();
+        runner.setId(UUID.randomUUID());
+        runner.setName(NAME);
+        return runner;
+    }
+}
