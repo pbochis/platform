773ae7ad10c7efd1f7099242ffc0be5a884b3f3c
Paul Bochis <paul.bochis@gmail.com>
2016-03-30 16:45:27 +0300

    add language to submission

--- platform-server-kernel/src/main/java/uno/cod/platform/server/core/repository/LanguageRepository.java
+++ platform-server-kernel/src/main/java/uno/cod/platform/server/core/repository/LanguageRepository.java
@@ -1,9 +1,15 @@
 package uno.cod.platform.server.core.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import uno.cod.platform.server.core.domain.Language;
 
 import java.util.UUID;
 
 public interface LanguageRepository extends JpaRepository<Language, UUID> {
+
+    @Query("Select language From Language language where language.tag=:tag")
+    Language findByTag(@Param("tag") String tag);
+
 }
