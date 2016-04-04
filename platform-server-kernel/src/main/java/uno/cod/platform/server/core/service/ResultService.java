0452bf7d6483bc223f34c005d9db877b82348997
Victor Balan <victor.balan93@gmail.com>
2016-04-04 16:46:22 +0300

    Finish result when the challenge ends

--- platform-server-kernel/src/main/java/uno/cod/platform/server/core/service/ResultService.java
+++ platform-server-kernel/src/main/java/uno/cod/platform/server/core/service/ResultService.java
@@ -1,8 +1,11 @@
 package uno.cod.platform.server.core.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.TaskScheduler;
 import org.springframework.stereotype.Service;
-import uno.cod.platform.server.core.domain.*;
+import uno.cod.platform.server.core.domain.Challenge;
+import uno.cod.platform.server.core.domain.Result;
+import uno.cod.platform.server.core.domain.User;
 import uno.cod.platform.server.core.dto.challenge.LeaderboardEntryDto;
 import uno.cod.platform.server.core.dto.result.ResultShowDto;
 import uno.cod.platform.server.core.dto.user.UserShowDto;
@@ -13,6 +16,7 @@ import uno.cod.platform.server.core.repository.ResultRepository;
 import javax.transaction.Transactional;
 import java.time.ZonedDateTime;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
@@ -21,48 +25,69 @@ import java.util.UUID;
 public class ResultService {
     private final ResultRepository repository;
     private final ChallengeRepository challengeRepository;
+    private final TaskScheduler taskScheduler;
 
     @Autowired
     public ResultService(ResultRepository repository,
-                         ChallengeRepository challengeRepository) {
+                         ChallengeRepository challengeRepository, TaskScheduler taskScheduler) {
         this.repository = repository;
         this.challengeRepository = challengeRepository;
+        this.taskScheduler = taskScheduler;
     }
 
-    public ResultShowDto save(UUID challengeId, User user){
-        Challenge challenge = challengeRepository.findOne(challengeId);
-        if (challenge == null){
+    public ResultShowDto save(UUID challengeId, User user) {
+        Challenge challenge = challengeRepository.findOneWithTemplate(challengeId);
+        if (challenge == null) {
             throw new IllegalArgumentException("challenge.invalid");
         }
         Result result = repository.findOneByUserAndChallenge(user.getId(), challengeId);
-        if(result!=null){
+        if (result != null) {
             throw new IllegalArgumentException("challenge.completed");
         }
         result = new Result();
         challenge.addResult(result);
         result.setStarted(ZonedDateTime.now());
         result.setUser(user);
+        result = repository.save(result);
+        final UUID resultId = result.getId();
 
-        return ResultMapper.map(repository.save(result));
+        Date setFinished = null;
+        if (challenge.getEndDate() != null) {
+            setFinished = Date.from(challenge.getEndDate().toInstant());
+        } else {
+            setFinished = Date.from(result.getStarted().plus(challenge.getChallengeTemplate().getDuration()).toInstant());
+        }
+
+        taskScheduler.schedule(() -> {
+                    Result r = repository.findOne(resultId);
+                    if (r.getFinished() != null) {
+                        return;
+                    }
+                    r.setFinished(ZonedDateTime.now());
+                    repository.save(r);
+                }
+                , setFinished);
+
+        return ResultMapper.map(result);
     }
 
-    public ResultShowDto findOne(UUID id){
+    public ResultShowDto findOne(UUID id) {
         return ResultMapper.map(repository.findOne(id));
     }
 
-    public ResultShowDto findOneByUserAndChallenge(UUID userId, UUID challengeId){
+    public ResultShowDto findOneByUserAndChallenge(UUID userId, UUID challengeId) {
         return ResultMapper.map(repository.findOneByUserAndChallenge(userId, challengeId));
     }
 
     public List<LeaderboardEntryDto> getLeaderboard(UUID challengeId) {
         List<LeaderboardEntryDto> leaderboard = new ArrayList<>();
         List<Object[]> results = repository.findLeaderboardForChallenge(challengeId);
-        for (Object[] resultEntry: results){
+        for (Object[] resultEntry : results) {
             LeaderboardEntryDto entry = new LeaderboardEntryDto();
-            Result result = (Result)resultEntry[0];
+            Result result = (Result) resultEntry[0];
             entry.setUser(new UserShowDto(result.getUser()));
-            entry.setTasksCompleted((Long)resultEntry[1]);
-            entry.setLastLevelFinishDate((ZonedDateTime)resultEntry[2]);
+            entry.setTasksCompleted((Long) resultEntry[1]);
+            entry.setLastLevelFinishDate((ZonedDateTime) resultEntry[2]);
             leaderboard.add(entry);
         }
         return leaderboard;
