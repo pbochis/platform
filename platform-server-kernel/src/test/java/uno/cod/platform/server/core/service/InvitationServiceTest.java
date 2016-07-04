d14892dc963676c7a060b08327eef01f1c926c77
Lorenz Leutgeb <lorenz.leutgeb@cod.uno>
2016-07-04 19:47:53 +0200

    Add two tests for InvitationService

--- platform-server-kernel/src/test/java/uno/cod/platform/server/core/service/InvitationServiceTest.java
+++ platform-server-kernel/src/test/java/uno/cod/platform/server/core/service/InvitationServiceTest.java
@@ -1,6 +1,5 @@
 package uno.cod.platform.server.core.service;
 
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
@@ -8,6 +7,7 @@ import uno.cod.platform.server.core.domain.Invitation;
 import uno.cod.platform.server.core.domain.Result;
 import uno.cod.platform.server.core.domain.User;
 import uno.cod.platform.server.core.dto.invitation.InvitationShowDto;
+import uno.cod.platform.server.core.exception.CodunoAccessDeniedException;
 import uno.cod.platform.server.core.repository.ChallengeRepository;
 import uno.cod.platform.server.core.repository.InvitationRepository;
 import uno.cod.platform.server.core.repository.ResultRepository;
@@ -18,8 +18,13 @@ import uno.cod.platform.server.core.service.util.ResultTestUtil;
 import uno.cod.platform.server.core.service.util.UserTestUtil;
 
 import javax.servlet.http.HttpSession;
+import java.time.Instant;
+import java.time.ZoneId;
 import java.util.Collections;
 import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
 
 public class InvitationServiceTest {
     private InvitationService invitationService;
@@ -53,20 +58,44 @@ public class InvitationServiceTest {
         User user = UserTestUtil.getUser();
         Result result = ResultTestUtil.getResult();
 
-        Mockito.when(invitationRepository.findAllByChallenge(invitation.getChallenge().getId())).thenReturn(Collections.singletonList(invitation));
-        Mockito.when(userRepository.findByUsernameOrEmail(invitation.getEmail(), invitation.getEmail())).thenReturn(user);
-        Mockito.when(resultRepository.findOneByUserAndChallenge(user.getId(), invitation.getChallenge().getId())).thenReturn(result);
+        when(invitationRepository.findAllByChallenge(invitation.getChallenge().getId())).thenReturn(Collections.singletonList(invitation));
+        when(userRepository.findByUsernameOrEmail(invitation.getEmail(), invitation.getEmail())).thenReturn(user);
+        when(resultRepository.findOneByUserAndChallenge(user.getId(), invitation.getChallenge().getId())).thenReturn(result);
 
         List<InvitationShowDto> dtos = invitationService.getByChallengeId(invitation.getChallenge().getId());
 
-        Assert.assertEquals(dtos.size(), 1);
+        assertEquals(dtos.size(), 1);
 
         InvitationShowDto dto = dtos.get(0);
 
-        Assert.assertEquals(dto.getEmail(), invitation.getEmail());
-        Assert.assertEquals(dto.getUsername(), user.getUsername());
-        Assert.assertEquals(dto.getToken(), invitation.getToken());
-        Assert.assertEquals(dto.getStarted(), result.getStarted());
-        Assert.assertEquals(dto.getExpire(), invitation.getExpire());
+        assertEquals(dto.getEmail(), invitation.getEmail());
+        assertEquals(dto.getUsername(), user.getUsername());
+        assertEquals(dto.getToken(), invitation.getToken());
+        assertEquals(dto.getStarted(), result.getStarted());
+        assertEquals(dto.getExpire(), invitation.getExpire());
+    }
+
+    @Test(expected = CodunoAccessDeniedException.class)
+    public void authByExpiredToken() throws Exception {
+        final String token = "iamexpired";
+
+        final Invitation invitation = new Invitation();
+
+        // Let's hope nobody travels back in time before
+        // the seventies and executes this unit test ...
+        invitation.setExpire(Instant.EPOCH.atZone(ZoneId.of("UTC")));
+
+        when(invitationRepository.getOne(token)).thenReturn(invitation);
+
+        invitationService.authByToken(token);
+    }
+
+    @Test(expected = CodunoAccessDeniedException.class)
+    public void authByNonexistentToken() throws Exception {
+        final String token = "mimistaccio";
+
+        when(invitationRepository.getOne(token)).thenReturn(null);
+
+        invitationService.authByToken(token);
     }
 }
