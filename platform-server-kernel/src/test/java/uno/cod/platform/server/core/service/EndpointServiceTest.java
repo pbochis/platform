d01abe11e70504c6ec627e0be0b98e22e614b9eb
Lorenz Leutgeb <lorenz.leutgeb@cod.uno>
2016-07-04 19:16:29 +0200

    Test EndpointService

--- platform-server-kernel/src/test/java/uno/cod/platform/server/core/service/EndpointServiceTest.java
+++ platform-server-kernel/src/test/java/uno/cod/platform/server/core/service/EndpointServiceTest.java
@@ -1,16 +1,23 @@
 package uno.cod.platform.server.core.service;
 
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
 import uno.cod.platform.server.core.domain.Endpoint;
+import uno.cod.platform.server.core.dto.endpoint.EndpointCreateDto;
 import uno.cod.platform.server.core.dto.endpoint.EndpointShowDto;
 import uno.cod.platform.server.core.repository.EndpointRepository;
 import uno.cod.platform.server.core.service.util.EndpointTestUtil;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.UUID;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 public class EndpointServiceTest {
     private EndpointService service;
@@ -26,15 +33,58 @@ public class EndpointServiceTest {
     public void findAll() throws Exception {
         List<Endpoint> endpoints = Collections.singletonList(EndpointTestUtil.getEndpoint());
 
-        Mockito.when(repository.findAll()).thenReturn(endpoints);
+        when(repository.findAll()).thenReturn(endpoints);
 
         List<EndpointShowDto> dtos = service.findAll();
 
-        Assert.assertEquals(dtos.size(), endpoints.size());
+        assertEquals(dtos.size(), endpoints.size());
         Endpoint endpoint = endpoints.get(0);
         EndpointShowDto dto = dtos.get(0);
-        Assert.assertEquals(endpoint.getId(), dto.getId());
-        Assert.assertEquals(endpoint.getName(), dto.getName());
-        Assert.assertEquals(endpoint.getComponent(), dto.getComponent());
+        assertEquals(endpoint.getId(), dto.getId());
+        assertEquals(endpoint.getName(), dto.getName());
+        assertEquals(endpoint.getComponent(), dto.getComponent());
+    }
+
+    @Test
+    public void createFromDto() throws Exception {
+        final String name = "name", component = "component";
+
+        final EndpointCreateDto dto = new EndpointCreateDto();
+        dto.setComponent(component);
+        dto.setName(name);
+
+        final Endpoint endpoint = new Endpoint();
+        endpoint.setComponent(component);
+        endpoint.setName(name);
+
+        when(repository.save(any(Endpoint.class))).thenReturn(endpoint);
+
+        ArgumentCaptor<Endpoint> captor = ArgumentCaptor.forClass(Endpoint.class);
+        service.createFromDto(dto);
+
+        verify(repository).save(captor.capture());
+
+        assertEquals(component, captor.getValue().getComponent());
+        assertEquals(name, captor.getValue().getName());
+    }
+
+    @Test
+    public void createFromExistingDto() throws Exception {
+        final String component = "component";
+
+        final EndpointCreateDto dto = new EndpointCreateDto();
+        dto.setComponent(component);
+
+        final UUID uuid = new UUID(0xcafe, 0xbabe);
+
+        final Endpoint endpoint = new Endpoint();
+        endpoint.setComponent(component);
+        endpoint.setId(uuid);
+
+        when(repository.findOneByComponent(component)).thenReturn(endpoint);
+
+        final UUID result = service.createFromDto(dto);
+
+        assertEquals(uuid, result);
     }
 }
